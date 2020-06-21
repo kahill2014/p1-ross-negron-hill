9b77aece0d004ba27104911289d75e1d21de06c3
Kevin Hill <kahill2014@gmail.com>
2020-06-20 22:18:26 -0500

    Update contact page to add styles and modal functionality

--- js/main.js
+++ js/main.js
@@ -10,7 +10,30 @@ $("#cater-submit").click(function(){
   $("#cater-ok").show();
 });
 
-// Hide modal when "Got it!" button is clicked from the thank you message
+// Show thank you modal when the contact form is submitted
+$("#contact-submit").click(function(){
+  $("#contact-modal .modal-body").text("Thank you for your submission! We will reach out to you shortly.");
+  $("#contact-modal").show();
+});
+
+// Hide modal when "Got it!" button is clicked from the thank you message for catering
 $("#cater-ok").click(function(){
   $("#cater-modal").modal("hide");
 });
+
+// Hide modal when "Got it!" button is clicked from the thank you message for contact
+$("#contact-ok").click(function(){
+  $("#contact-modal").modal("hide");
+});
+
+// Update remaining characters for contact form message
+$("textarea").keyup(function(){
+  var remaining = 255 - $("textarea").val().length;
+  if (remaining <= 0){
+    $("#excess-chars").show();
+    $("#chars-remaining").text("0");
+  } else {
+    $("#excess-chars").hide();
+    $("#chars-remaining").text(remaining);
+  }
+})
